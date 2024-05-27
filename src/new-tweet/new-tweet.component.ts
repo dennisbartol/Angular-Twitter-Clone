6d95ed852d5c13117f753574283fa88333fe29f0
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-05-27 16:16:40 +0200

    Update new-tweet.component.ts

--- src/new-tweet/new-tweet.component.ts
+++ src/new-tweet/new-tweet.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
 import { TweetService } from '../services/tweet.service';
 
+@Component({
+  selector: 'app-new-tweet',
+  templateUrl: './new-tweet.component.html',
+  styleUrls: ['./new-tweet.component.css']
+})
 
