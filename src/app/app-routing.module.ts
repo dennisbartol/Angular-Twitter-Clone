e862dd33bd5e2e02943bb814e1c309ed185677e1
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-05-28 12:55:03 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,4 +4,9 @@ import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AuthComponent } from './auth/auth.component';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'auth', component: AuthComponent },
+];
 
