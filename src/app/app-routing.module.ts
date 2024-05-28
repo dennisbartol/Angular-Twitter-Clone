3e83818bb51a5efed1d7212d746b01df672d6a69
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-05-28 12:53:33 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,1 +1,7 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AuthComponent } from './auth/auth.component';
+
 
