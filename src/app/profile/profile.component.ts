2c3b66240928c59fbbb6d266c9a0aeab4afa5f43
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-05-28 13:11:00 +0200

    Update profile.component.ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,2 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
