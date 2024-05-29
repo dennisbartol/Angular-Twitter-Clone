455755db07764467304224dcb45d571b8fd54593
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-05-29 16:06:13 +0200

    Update auth.component.ts

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,4 +2,8 @@ import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 
-
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
