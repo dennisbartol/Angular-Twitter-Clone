fd696f0205a2d44c8ab445f047caea2995055e80
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-05-30 20:55:49 +0200

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -26,7 +26,8 @@ export class AuthService {
 
 
   login(email: string, password: string): Observable<any> {
-  
+    
+    return this.http.post<any>(`${this.apiUrl}/login`, { email, password })
   
   }
   
