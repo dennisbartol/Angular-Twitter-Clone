90fd926d29bed9b3cbcd7e46342747433e992a73
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-05-30 20:20:55 +0200

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,1 +1,7 @@
+// A service to handle authentication logic 
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
