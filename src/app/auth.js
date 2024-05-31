325bfa7998f41c8f0a3e4cd41bd8cb4906dc8ba2
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-05-31 14:11:52 +0200

    Update auth.js

--- src/app/auth.js
+++ src/app/auth.js
@@ -8,3 +8,14 @@ router.post('/signup', async (req, rest) => {
   const { email, password } = req.body; 
   const hashedPassword = await bcrypt.hash(password, 10);
   const user = new User ({ email, password: hashedPassword });
+
+try { 
+  const newUser = await user.save();
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  res.status(201).json({ user: newUser, token });
+}
+  catch(err) {
+    res.status(400),json({ message: err.message });
+  }
+});
+
