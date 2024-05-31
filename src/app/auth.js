fb07ee3ed0f41817c818f934aa5af3d32e1a9c7a
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-05-31 14:09:31 +0200

    Update auth.js

--- src/app/auth.js
+++ src/app/auth.js
@@ -2,3 +2,9 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+
+// Signup
+router.post('/signup', async (req, rest) => {
+  const { email, password } = req.body; 
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User ({ email, password: hashedPassword });
